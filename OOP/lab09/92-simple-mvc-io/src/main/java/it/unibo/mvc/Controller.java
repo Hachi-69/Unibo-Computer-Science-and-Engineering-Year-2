d2b43fd18ecebef9385a5b3a4e7e8d6cd5e3e584
Luca Turillo <luca.turillo@studio.unibo.it>
2025-11-18 16:33:13 +0100

    comleted the Controller and the SimpleGUI classes, TODO implement SimpleGUIWithFileChooser

--- OOP/lab09/92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ OOP/lab09/92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,37 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Objects;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+final public class Controller {
 
+    private File currentFile = new File(
+            System.getProperty("user.home")
+            + System.getProperty("file.separator")
+            + "output.txt"
+    );
+
+    public void setCurrentFile(final File file) {
+        Objects.requireNonNull(file);
+        this.currentFile = file;
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    public String getCurrentFilePath() {
+        return this.currentFile.getPath();
+    }
+
+    public void write(final String content) throws IOException {
+        try (final FileWriter w = new FileWriter(this.currentFile)) {
+            w.write(content);
+        }
+    }
 }
